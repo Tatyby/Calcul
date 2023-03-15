ad031e0eaf8e873444720fa448b7969a2068eb17
Tatiana <in_sdo@mail.ru>
2023-03-15 09:39:12 +0300

    using the ternary operator

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,14 +5,8 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) ->
-    {
-        if (y == 0) {
-            System.out.println("Деление на 0 невозможно");
-        }
-        return x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y!=0?x/y:0;
 
-    };
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
